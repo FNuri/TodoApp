46d04a0727620f542334e38399617c920c7d21de
Fowsi <fowsihashi@hotmail.co.uk>
2020-04-04 01:33:14 +0300

    My first commit.

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ let sanitizeHTML = require ('sanitize-html')
 let app = express ()
 let db
 
-let port = Process.env.PORT || 3000
-app.use(express.static('/public'))
+let port = Process.env.PORT
+app.use(express.static('public'))
 
 let connectionString = 'mongodb+srv://todoAppUser:Abs123@123@cluster0-xw8k8.mongodb.net/todoApp?retryWrites=true&w=majority'
 mongodb.connect (connectionString, {useNewUrlParser: true}, function (err, client) {
