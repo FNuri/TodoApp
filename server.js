61db8f69ff1d99c71ce867efc549127ecf02325c
Fowsi <fowsihashi@hotmail.co.uk>
2020-04-18 03:22:51 +0300

    Added link to CSS in html

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ app.get ('/', function (req, res) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Simple To-Do App</title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    </head>
+      <link href="./main.css" rel="stylesheet" type="text/css">
+      </head>
     <body>
 
     <nav class="site-nav">
