bf6f5ff540b76f0da3d88427d4cff6781cc0b5f6
Fowsi <fowsihashi@hotmail.co.uk>
2020-05-04 01:54:45 +0300

    Changed nav links

--- server.js
+++ server.js
@@ -47,10 +47,10 @@ app.get ('/', function (req, res) {
 
     <nav class="site-nav">
       <ul style= "list-style-type:none" class="group">
-        <li><a href="https://fanurtechnology.herokuapp.com/">Home</a></li>
-        <li><a href="https://fanurtechnology.herokuapp.com/portfolio.html">Portfolio</a></li>
-        <li><a href="https://fanurtechnology.herokuapp.com/about.html">About</a></li>
-        <li><a href="https://fanurtechnology.herokuapp.com/contactus.html">Contact Us</a></li>
+        <li><a href="https://fowsinur.com/">Home</a></li>
+        <li><a href="https://fowsinur.com/portfolio.html">Portfolio</a></li>
+        <li><a href="https://fowsinur.com/about.html">About</a></li>
+        <li><a href="https://fowsinur.com/contactus.html">Contact Us</a></li>
       </ul>
     </nav>
       <div class="container">
